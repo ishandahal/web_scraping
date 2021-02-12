c8c9473991c8080ae554aed6edc8eb838278b942
ishan dahal <ishan.dahal88@gmail.com>
2021-02-11 20:50:59 -0500

    Start a web scraper
    Started a web scraper. At the moment it can go
    to timejobs.com and grab job titles, key skills and
    link to more details.

--- job_scraper.py
+++ job_scraper.py
@@ -1,10 +1,26 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
-page = requests.get("https://www.indeed.com/jobs?q=Entry%20Level%20Machine%20Learning&l&ts=1609087830269&rq=1&rsIdx=3&fromage=last&newcount=769&advn=8876452989351355&vjk=8759116ae6756433").text
-soup = BeautifulSoup(page, 'html.parser')
+url = "https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation="
+html_text = requests.get(url).text
+soup = BeautifulSoup(html_text, "html.parser")
+
+job_dict = {}
+
+# collects all individual cards
+containers = soup.findAll('li', class_="clearfix job-bx wht-shd-bx")
+# iterate each card and grab job title, key skills and link to info
+# create a dict with the above information
+for container in containers:
+    job_title = container.find("h3", class_="joblist-comp-name").text.strip()
+    key_skills = container.find("span", class_="srp-skills").text.strip().split(",")
+    link_with_details = container.h2.a['href']
+    job_dict[job_title] = key_skills
+    job_dict[job_title].append(link_with_details)
+
+print(len(job_dict))
+print(job_dict["3RI Technologies Pvt Ltd"])
 
 
-company_name = soup.find('div', class_="sjcl").text.lstrip()
-print(company_name)
+
 
